589d963ce1896dcd14ede6c5a32852cdaf173fe9
six519 <ferdinandsilva@ferdinandsilva.com>
2013-03-10 12:44:12 +0800

    fixed bug in class loader

--- sf/sf.php
+++ sf/sf.php
@@ -7,12 +7,17 @@ use sf\core\http\Http404;
 
 function __autoload($classname) {
 
-	$fileToLoad = strtolower(str_replace("\\", "/",$classname)) . ".php";
+	$fileToLoad = str_replace("\\", "/",$classname) . ".php";
+	$fileToLoadAllLower = strtolower($fileToLoad);
 
-	if(file_exists($fileToLoad)) {
-		include_once($fileToLoad);
+	if(file_exists($fileToLoadAllLower)) {
+		include_once($fileToLoadAllLower);
 	} else {
-		$http404 = new Http404();
+		if(file_exists($fileToLoad)) {
+			include_once($fileToLoad);
+		} else {
+			$http404 = new Http404();
+		}
 	}
 	
 }
